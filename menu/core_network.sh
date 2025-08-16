e0264af0aa09a8ba0b6ab68ec5b9de771e331d33
GertGerber <ggerber@outlook.co.nz>
2025-08-16 18:57:37 +1200

    On branch main Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	renamed:    menu/ansible_menu.sh -> menu/core_network.sh
    	modified:   menu/menu.sh
    	new file:   modules/ssh_keys/inventory.yml
    	new file:   modules/ssh_keys/playbook.yml
    	new file:   modules/ssh_keys/roles/ssh_key_deploy/defaults/main.yml
    	new file:   modules/ssh_keys/roles/ssh_key_deploy/tasks/generate_key.yml
    	new file:   modules/ssh_keys/roles/ssh_key_deploy/tasks/push_key.yml

--- menu/core_network.sh
+++ menu/core_network.sh
@@ -2,19 +2,23 @@
 . "$MENU_DIR/config.sh"
 . "$MENU_DIR/utils.sh"
 
-ansible_menu() {
+core_network_menu() {
     while true; do
         CHOICE=$(dialog --clear --backtitle "$BACKTITLE" \
             --title "Ansible Playbooks" \
             --menu "Select a playbook to run:" 15 50 5 \
-            1 "Setup servers (setup.yml)" \
-            2 "Deploy application (deploy.yml)" \
-            3 "Back to Main Menu" 3>&1 1>&2 2>&3)
+            1 "Setup DHCP Servers" \
+            2 "Setup DNS Servers" \
+            3 "Setup AD Servers" \
+            4 "Setup ADBLOCK Servers"
+            5 "Back to Main Menu" 3>&1 1>&2 2>&3)
 
         case $CHOICE in
             1) ansible-playbook "$PLAYBOOKS_DIR/setup.yml"; pause ;;
             2) ansible-playbook "$PLAYBOOKS_DIR/deploy.yml"; pause ;;
-            3) break ;;
+            3) ansible-playbook "$PLAYBOOKS_DIR/ad.yml"; pause ;;
+            4) ansible-playbook "$PLAYBOOKS_DIR/adblock.yml"; pause ;;
+            5) break ;;
             *) break ;;
         esac
     done
--- menu/menu.sh
+++ menu/menu.sh
@@ -6,21 +6,26 @@ MENU_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Load config & utilities
 . "$MENU_DIR/config.sh"
 . "$MENU_DIR/utils.sh"
-. "$MENU_DIR/ansible_menu.sh"
+. "$MENU_DIR/core_network.sh"
 . "$MENU_DIR/terraform_menu.sh"
 
 while true; do
     CHOICE=$(dialog --clear --backtitle "$BACKTITLE" \
         --title "Main Menu" \
         --menu "Choose an option:" 15 50 4 \
-        1 "Ansible Playbooks" \
-        2 "Terraform Commands" \
-        3 "Exit" 3>&1 1>&2 2>&3)
+        1 "Install SSh-Keys" \
+        2 "Setup CoreNetwork" \
+        3 "Run Terraform Commands" \
+        4 "Exit" 3>&1 1>&2 2>&3)    
 
     case $CHOICE in
-        1) ansible_menu ;;
-        2) terraform_menu ;;
-        3) clear; exit ;;
+        1)  
+            TARGET_DIR="$HOME/Github/Proxmox_homeLab_v3"
+            cd "$TARGET_DIR/modules/ssh_keys"
+            ansible-playbook -i inventory.yml playbook.yml; pause ;;
+        2) core_network_menu ;;
+        3) terraform_menu ;;
+        4) clear; exit ;;
         *) break ;;
     esac
 done
